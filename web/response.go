2183080a5ee2bb84c7cf5024a7c044bb4120eda0
otumian-empire <popecan1000@gmail.com>
2023-08-21 21:22:01 +0000

    implement response

---
+++ web/response.go
@@ -0,0 +1,54 @@
+package web
+
+import "net/http"
+
+// generic data
+type T map[string]any
+
+type response struct {
+	status int
+	others T
+}
+
+func new(statusCode int, others T) *response {
+	return &response{
+		status: statusCode,
+		others: others,
+	}
+}
+
+func (r *response) send() (int, T) {
+	return r.status, r.others
+}
+
+func SuccessResponse(message string, data any) (int, T) {
+	return new(http.StatusOK, T{
+		"success": true,
+		"message": message,
+		"data":    data,
+	}).send()
+}
+
+func SuccessMessageResponse(message string) (int, T) {
+	return new(http.StatusOK, T{
+		"success": true,
+		"message": message,
+		"data":    nil,
+	}).send()
+}
+
+func FailureMessageResponse(message string) (int, T) {
+	return new(http.StatusOK, T{
+		"success": false,
+		"message": message,
+		"data":    nil,
+	}).send()
+}
+
+func ErrorResponse(message string) (int, T) {
+	return new(http.StatusInternalServerError, T{
+		"success": false,
+		"message": message,
+		"data":    nil,
+	}).send()
+}
