40a57550c0ce538bc407b16a4c54fb9596d4894d
otumian-empire <popecan1000@gmail.com>
2023-08-22 20:22:02 +0000

    implement sign up and login body

--- web/user.go
+++ web/user.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/otumian-empire/go-to-do-list-restful-api/model"
 )
@@ -9,9 +11,59 @@ type UserController struct {
 	model model.UserModel
 }
 
-func (controller *UserController) SignUp() gin.HandlerFunc
+func (controller *UserController) SignUp() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// get the data from the request body
+		var payload AuthRequestBody
 
-func (controller *UserController) Login() gin.HandlerFunc
+		if err := context.BindJSON(&payload); err != nil {
+			log.Println(err.Error())
+			context.JSON(FailureMessageResponse(err.Error()))
+			return
+		}
+
+		// TODO: install bcrypt and create a hash of the password
+
+		// create user
+		if err := controller.model.CreateUser(payload.Username, payload.Password); err != nil {
+			context.JSON(FailureMessageResponse(err.Error()))
+			return
+		}
+
+		// return success message on user creation
+		log.Println(SIGN_UP_SUCCESSFUL)
+		context.JSON(SuccessMessageResponse(SIGN_UP_SUCCESSFUL))
+	}
+}
+
+func (controller *UserController) Login() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// get the data from the request body
+		var payload AuthRequestBody
+
+		if err := context.BindJSON(&payload); err != nil {
+			log.Println(err.Error())
+			context.JSON(FailureMessageResponse(err.Error()))
+			return
+		}
+
+		// read row with the same username
+		row, err := controller.model.ReadUserByUsername(payload.Username)
+
+		if err != nil {
+			log.Println(err.Error())
+			context.JSON(FailureMessageResponse(err.Error()))
+			return
+		}
+
+		// TODO: compare the user password passed to the hash
+		row.Password = ""
+
+		// return success message on user creation
+		log.Println(LOGIN_SUCCESSFUL)
+		context.JSON(SuccessResponse(LOGIN_SUCCESSFUL, row))
+	}
+}
 
 func (controller *UserController) UpdateUserUsername() gin.HandlerFunc
 
