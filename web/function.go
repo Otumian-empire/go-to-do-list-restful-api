c991b4840d0aa9ee8c287746bc622c1bb2c114f3
otumian-empire <popecan1000@gmail.com>
2023-09-22 14:40:01 +0000

    refactor the validation functions

--- web/function.go
+++ web/function.go
@@ -1,10 +1,7 @@
 package web
 
 import (
-	"fmt"
 	"log"
-	"strconv"
-	"strings"
 )
 
 func Recover() {
@@ -12,37 +9,4 @@ func Recover() {
 		log.Println(SERVER_RECOVER_FROM_ERROR)
 		log.Println(err)
 	}
-}
-
-func ConvertStringIdToInt(id string) (int, error) {
-	if len(id) < 1 {
-		return 0, fmt.Errorf(INVALID_ID)
-	}
-
-	intId, intIdErr := strconv.Atoi(id)
-
-	if intIdErr != nil {
-		log.Println(intIdErr)
-		return 0, fmt.Errorf(INVALID_ID)
-	}
-
-	return intId, nil
-}
-
-func ValidateString(value string) (validatedString string, isValidString bool) {
-	validatedString = strings.Trim(value, " ")
-	isValidString = len(validatedString) > 1
-
-	return validatedString, isValidString
-
-}
-
-func convertStringQueryToInt(value string, defaultValue int) int {
-	intValue, valueError := strconv.Atoi(value)
-
-	if valueError != nil {
-		intValue = defaultValue
-	}
-
-	return intValue
 }
---
+++ web/validation.go
@@ -0,0 +1,41 @@
+package web
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func ConvertStringIdToInt(id string) (int, error) {
+	if len(id) < 1 {
+		return 0, fmt.Errorf(INVALID_ID)
+	}
+
+	intId, intIdErr := strconv.Atoi(id)
+
+	if intIdErr != nil {
+		log.Println(intIdErr)
+		return 0, fmt.Errorf(INVALID_ID)
+	}
+
+	return intId, nil
+}
+
+func ValidateString(value string) (validatedString string, isValidString bool) {
+	validatedString = strings.Trim(value, " ")
+	isValidString = len(validatedString) > 1
+
+	return validatedString, isValidString
+
+}
+
+func convertStringQueryToInt(value string, defaultValue int) int {
+	intValue, valueError := strconv.Atoi(value)
+
+	if valueError != nil {
+		intValue = defaultValue
+	}
+
+	return intValue
+}
